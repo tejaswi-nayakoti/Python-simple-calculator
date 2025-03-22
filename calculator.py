2be72954a5aa53d379d7d2ef430988ff33a05d77
tejaswi-nayakoti <nayakotitejaswi5@gmail.com>
2025-03-22 15:20:03 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,55 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x,y):
+  if y !=0:
+    return x / y
+  else:
+    return "Error! Division by zero."
+
+def calculator():
+    print("Simple Calculator")
+    print("Choose operation:")
+    print("1. Add")
+    print("2.Subtract")
+    print("3.Multiply")
+    print("4.Divide")
+
+    while True:
+         choice = input("Enter choice (1/2/3/4): ")
+
+         if choice in ('1', '2', '3', '4'):
+             num1 = float(input("Enter first number: "))
+             num2 = float(input("Enter second number: "))
+
+             if choice == '1':
+                 print(f"The Result is: {add(num1, num2)}")
+             elif choice == '2':
+                 print(f"The Result is: {subtract(num1, num2)}")
+             elif choice == '3':
+                 print(f"The Result is: {multiply(num1, num2)}")
+             elif choice == '4':
+                 print(f"The Result is: {divide(num1, num2)}")
+
+             next_calculation = input("Do you want to perform another calculation? (yes/no): ")
+             if next_calculation.lower() != 'yes':
+                 break
+         else:
+             print("Invalid input")
+
+#Run the calculator
+calculator()
